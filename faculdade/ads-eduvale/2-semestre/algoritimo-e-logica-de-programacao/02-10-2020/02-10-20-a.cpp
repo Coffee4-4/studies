5c041dd52597212a197762ff3b1bcb894071bc47
André Melicio <nybble94@gmail.com>
2020-11-05 17:09:41 -0300

    1

--- faculdade/ads-eduvale/2-semestre/algoritimo-e-logica-de-programacao/02-10-2020/02-10-20-a.cpp
+++ faculdade/ads-eduvale/2-semestre/algoritimo-e-logica-de-programacao/02-10-2020/02-10-20-a.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 int main(){
 	int numero = 0;
-	
+
 	cout << "Informe um número: " << endl;
 	cin >> numero;
-	
+
 	switch(numero){
-		
+
 		case 1: cout << "Numero um" << endl; break;
 		case 2: cout << "Numero dois" << endl; break;
 		case 3: cout << "Numero tres" << endl; break;
 		case 4: cout << "Numero quatro" << endl; break;
 		default: cout << "Numero Inexistente" << endl;
-	}		
 	}
-	
+
+
 	return 0;
 }
 
--- faculdade/ads-eduvale/2-semestre/algoritimo-e-logica-de-programacao/05-11-2020/teste.cpp
+++ faculdade/ads-eduvale/2-semestre/algoritimo-e-logica-de-programacao/05-11-2020/teste.cpp
@@ -1,4 +1,20 @@
-//
-// Created by andre on 05/11/2020.
-//
+#include <iostream>
+#include <locale.h>
+using namespace std;
 
+
+int main (){
+
+    char letra;
+    cout<< "Informe um letra: "<< endl;
+    cin >> letra;
+
+    switch(letra){
+
+        case 'q':
+            cout << "Sair do Sistema" << endl; exit(1);
+        case 'Q':
+            cout << "Sair do Sistema" << endl; exit(1);
+    }
+
+}
